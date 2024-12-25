3c55ee4fe9e278f5e34501fa9474daa1b569aca8
nbngoc123 <nbngoc.udu.ptit@gmail.com>
2024-12-25 12:14:41 +0700

    up

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,9 +15,9 @@ const App = () => {
   const [showMore, setShowMore] = useState(false); // State để quản lý nội dung thêm
 
   const slideImages = [
-    'https://via.placeholder.com/1200x400?text=Slide+1',
-    'https://via.placeholder.com/1200x400?text=Slide+2',
-    'https://via.placeholder.com/1200x400?text=Slide+3',
+    require('./i1.jpg'),
+    require('./i2.jpg'),
+    require('./i3.jpg'),
   ];
 
   // Hàm xử lý khi nhấn "Learn More"
